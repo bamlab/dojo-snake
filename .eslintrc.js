8ffe744127a0be38f466801ce2ed660de7c34dac
Louis Kraemer <louisk@bam.tech>
2020-01-09 20:47:03 +0100

    :heavy_plus_sign: (eslint)

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: "airbnb",
+  parser: "babel-eslint",
+  rules: {
+    "react/jsx-filename-extension": "off",
+    "react/prop-types": "off",
+  }
+};
--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, View, StatusBar,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar hidden />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  container: { flex: 1 },
 });
