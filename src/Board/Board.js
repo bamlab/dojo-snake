e3c9d4a267991b68b13e88411140d821fc7c8015
Louis Kraemer <louisk@bam.tech>
2020-01-09 21:25:35 +0100

    :sparkles: (Board) add board and cell

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -2,14 +2,35 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useBoard } from './useBoard';
 
-export const Board = ({ height, width }) => {
-  const {} = useBoard();
+const Cell = ({ size }) => (
+  <View style={[styles.cell, {
+    height: size, width: size,
+  }]}
+  />
+);
 
-  return (<View style={[styles.container, { height, width }]} />);
+export const Board = ({ height, width }) => {
+  const { cellSize, boardSize } = useBoard({ height, width });
+
+  return (
+    <View style={[styles.container, {
+      height, width,
+    }]}
+    >
+      <View style={[styles.board, { height: boardSize, width: boardSize }]}><Cell size={cellSize} /></View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  board: {
     backgroundColor: 'black',
+  },
+  cell: {
+    backgroundColor: 'green', position: 'absolute',
   },
 });
--- src/Board/useBoard.js
+++ src/Board/useBoard.js
@@ -1,1 +1,7 @@
-export const useBoard = () => ({});
+export const useBoard = ({ height, width }) => {
+  const boardSize = Math.min(height, width);
+  const GRID_SIZE = 15;
+  const cellSize = boardSize / GRID_SIZE;
+
+  return { cellSize, boardSize };
+};
