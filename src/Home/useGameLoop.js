de64296980907ba55edbf393ad6ec6dddd3583e5
Louis Kraemer <louisk@bam.tech>
2020-01-13 13:58:38 +0100

    :sparkles: add game over and retry

--- src/Home/useGameLoop.js
+++ src/Home/useGameLoop.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import { GRID_SIZE } from '../constants';
 
 const LOOP_INTERVAL = 200;
 
@@ -10,6 +12,19 @@ export const useGameLoop = () => {
   const [tail, setTail] = useState(initialTail);
   const [xSpeed, setXSpeed] = useState(1);
   const [ySpeed, setYSpeed] = useState(0);
+
+  const reset = () => {
+    setRunning(false);
+    setHead(initialHead);
+    setTail(initialTail);
+    setXSpeed(1);
+    setYSpeed(0);
+    Alert.alert('Game over', 'You lame loser', [{
+      text: 'Retry',
+      onPress: () => setRunning(true),
+    }]);
+  };
+
   const goLeft = () => {
     if (xSpeed !== -1 && xSpeed !== 1) {
       setXSpeed(-1);
@@ -39,6 +54,11 @@ export const useGameLoop = () => {
 
   const computeNextHead = () => {
     if (running) {
+      const nextHead = { top: head.top + ySpeed, left: head.left + xSpeed };
+      if (nextHead.top >= GRID_SIZE || nextHead.left >= GRID_SIZE || nextHead.top < 0 || nextHead.left < 0) {
+        reset();
+        return;
+      }
       setTail((currentTail) => {
         const [_, ...rest] = currentTail;
         return [...rest, head];
