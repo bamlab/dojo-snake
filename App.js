8857b986486c6e1d775dec95c588b9c11e813249
Louis Kraemer <louisk@bam.tech>
2020-02-17 18:58:12 +0100

    :sparkles: add stack navigator

--- App.js
+++ App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import {
   StyleSheet, View, StatusBar,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from './src/Home/Home';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerShown: false,
+        }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
