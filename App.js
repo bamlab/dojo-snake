ac639a278aa182ed6b2e11ec9cd18a771a7fd025
Louis Kraemer <louisk@bam.tech>
2020-02-23 21:47:51 +0100

    :fire: (RNavigation)

--- App.js
+++ App.js
@@ -2,25 +2,14 @@ import React from 'react';
 import {
   StyleSheet, View, StatusBar,
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from './src/Home/Home';
 
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerShown: false,
-        }}
-        >
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Home />
     </View>
   );
 }
