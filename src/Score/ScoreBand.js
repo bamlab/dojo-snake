f3ed6aa6d219f5cf269d58108ddf0d2ba53837c0
Delphine Bugner <delphineb@bam.tech>
2022-01-08 16:34:11 +0100

    :sparkles: feat: (Score) add ScoreBand & Modal

---
+++ src/Score/ScoreBand.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export const ScoreBand = ({ score, name, setName }) => {
+  return (
+    <View style={styles.top}>
+      <View style={styles.user}>
+        <Text>Joueur : </Text>
+        <TextInput onChangeText={setName} value={name} />
+      </View>
+      <Text>Score : {score}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  top: {
+    flexDirection: 'row',
+    margin: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 2,
+  },
+  user: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+});
---
+++ src/Score/ScoreModal.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Button, Modal, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { NOKIA_COLOR } from '../theme';
+
+export const ScoreModal = ({ visible, close, scores }) => {
+  console.log(scores);
+  return (
+    <Modal visible={visible} animationType={'slide'}>
+      <SafeAreaView style={styles.modal}>
+        <Text style={styles.title}>Meilleurs scores</Text>
+        <ScrollView style={styles.list}>
+          {scores
+            .filter(({ score }) => score > 0)
+            .sort((score1, score2) => score2.score - score1.score)
+            .map(({ score, name, time }, index) => (
+              <Text style={styles.line} key={time}>
+                <Text>
+                  {index + 1} - {new Date(time).toLocaleTimeString('fr')} - {name} - {score}
+                </Text>
+              </Text>
+            ))}
+        </ScrollView>
+        <Button title={'Fermer les scores'} onPress={close} />
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: NOKIA_COLOR,
+    paddingVertical: 20,
+  },
+  list: {
+    flex: 1,
+    paddingHorizontal: 30,
+  },
+  line: {
+    flex: 1,
+    color: NOKIA_COLOR,
+    fontSize: 18,
+    paddingVertical: 10,
+    flexDirection: 'row',
+  },
+  name: {
+    flex: 1,
+  },
+});
