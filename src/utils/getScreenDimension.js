81e75492e6d5f5e531d06a116ce170178d9378fe
Delphine Bugner <delphineb@bam.tech>
2022-03-23 12:38:08 +0100

    :fire: (getScreenDimension) remove buggy screen dimension for web

--- src/utils/getScreenDimension.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const getScreenDimension = () => {
-  // Retrieve screen height and width
-  const { height: screenHeight, width: screenWidth } = Dimensions.get('screen');
-
-  return { screenHeight, screenWidth };
-};
