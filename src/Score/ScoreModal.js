fc19c06eb7aad3580ec8410892dabfe47d14e0c8
Delphine Bugner <delphineb@bam.tech>
2022-03-23 15:08:21 +0100

    :sparkles: (ScoreModal) fix design for web

--- src/Score/ScoreModal.js
+++ src/Score/ScoreModal.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import { Button, Modal, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
 import { NOKIA_COLOR } from '../theme';
 
 export const ScoreModal = ({ visible, close, scores }) => {
   return (
-    <Modal visible={visible} animationType={'slide'}>
-      <SafeAreaView style={styles.modal}>
-        <Text style={styles.title}>Meilleurs scores</Text>
-        <ScrollView style={styles.list}>
-          {scores
-            ?.filter(({ score }) => score > 0)
-            .sort((line1, line2) => line2.score - line1.score)
-            .map(({ score, name, time }, index) => (
-              <Text style={styles.line} key={time}>
-                <Text>
-                  {index + 1} - {new Date(time).toLocaleTimeString('fr')} - {name} - {score}
-                </Text>
-              </Text>
-            ))}
-        </ScrollView>
-        <Button title={'Fermer les scores'} onPress={close} />
-      </SafeAreaView>
-    </Modal>
+    <SafeAreaView style={styles.modal}>
+      <Text style={styles.title}>Meilleurs scores</Text>
+      <ScrollView style={styles.list}>
+        {scores
+          ?.filter(({ score }) => score > 0)
+          ?.sort((line1, line2) => line2.score - line1.score)
+          .map(({ score, name, time }, index) => (
+            <Text style={styles.line} key={time} numberOfLines={1}>
+              {index + 1} - {new Date(time).toLocaleTimeString('fr')} - {name} - {score}
+            </Text>
+          ))}
+      </ScrollView>
+      <Button title={'Fermer les scores'} onPress={close} />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   modal: {
-    flex: 1,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
     backgroundColor: 'black',
+    zIndex: 2,
   },
   title: {
     fontSize: 30,
@@ -42,11 +43,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
   },
   line: {
-    flex: 1,
     color: NOKIA_COLOR,
     fontSize: 18,
     paddingVertical: 10,
-    flexDirection: 'row',
   },
   name: {
     flex: 1,
